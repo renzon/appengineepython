8b9360a6d74c42c9b34e9cabeafc8b30bc3973c4
renzon <renzon@gmail.com>
2014-08-29 17:43:53 -0300

    Criação de grupo GERENTE  e uso do decoraor permissions

--- backend/appengine/routes/pagina.py
+++ backend/appengine/routes/pagina.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from gaecookie.decorator import no_csrf
-from gaepermission.decorator import login_not_required, login_required
+from gaepermission.decorator import permissions
 from config.template_middleware import TemplateResponse
+from permission_app.model import ADMIN, GERENTE
 
-@login_required
+
+@permissions(ADMIN, GERENTE)
 @no_csrf
 def index():
     class Curso(object):
--- backend/apps/permission_app/model.py
+++ backend/apps/permission_app/model.py
@@ -2,5 +2,7 @@
 from __future__ import absolute_import, unicode_literals
 
 ADMIN = 'ADMIN'
+GERENTE = 'GERENTE'
 
-ALL_PERMISSIONS_LIST = [ADMIN]
+ALL_PERMISSIONS_LIST = [ADMIN,
+                        GERENTE]
