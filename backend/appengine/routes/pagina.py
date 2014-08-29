583b7ffb33073e57f9b21d332660c46c65ade483
renzon <renzon@gmail.com>
2014-08-29 17:29:08 -0300

    /pagina com acesso restrito a usuário logados

--- backend/appengine/routes/pagina.py
+++ backend/appengine/routes/pagina.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from gaecookie.decorator import no_csrf
-from gaepermission.decorator import login_not_required
+from gaepermission.decorator import login_not_required, login_required
 from config.template_middleware import TemplateResponse
 
-
+@login_required
 @no_csrf
 def index():
     class Curso(object):
