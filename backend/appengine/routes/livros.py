8bab2b69ea6e0b961ac185f7c8ef87b04a7537e4
renzon <renzon@gmail.com>
2014-09-10 15:41:34 -0300

    Salvamento de livro sem validação

--- backend/appengine/routes/livros.py
+++ backend/appengine/routes/livros.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from decimal import Decimal
 
 from google.appengine.ext import ndb
 from config.template_middleware import TemplateResponse
@@ -8,6 +9,8 @@ from gaegraph.model import Node
 from gaeforms.ndb import property
 
 # Classes de Modelo
+from tekton import router
+
 
 class Livro(Node):
     titulo = ndb.StringProperty(required=True)
@@ -18,4 +21,11 @@ class Livro(Node):
 # Handler de requisições HTTP
 @no_csrf
 def form():
-    return TemplateResponse()
+    contexto = {'salvar_path': router.to_path(salvar)}
+    return TemplateResponse(contexto)
+
+
+def salvar(titulo, preco, **outras_propriedades):
+    preco = Decimal(preco)
+    livro = Livro(titulo=titulo, preco=preco)
+    livro.put()
