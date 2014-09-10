654f8296247246032c6c6f2172784fe0fd7e52d4
renzon <renzon@gmail.com>
2014-09-10 15:13:47 -0300

    Criação de formulário para livro

--- backend/appengine/routes/livros.py
+++ backend/appengine/routes/livros.py
@@ -2,12 +2,20 @@
 from __future__ import absolute_import, unicode_literals
 
 from google.appengine.ext import ndb
+from config.template_middleware import TemplateResponse
+from gaecookie.decorator import no_csrf
 from gaegraph.model import Node
 from gaeforms.ndb import property
 
+# Classes de Modelo
 
 class Livro(Node):
     titulo = ndb.StringProperty(required=True)
     preco = property.SimpleCurrency(required=True)
-    lancamento = ndb.DateProperty(required=True)
+    lancamento = ndb.DateProperty(auto_now_add=True)
 
+
+# Handler de requisições HTTP
+@no_csrf
+def form():
+    return TemplateResponse()
