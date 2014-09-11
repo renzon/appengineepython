8b664ccb2b995b4251405539e1011c041d5032c5
renzon <renzon@gmail.com>
2014-09-11 08:47:54 -0300

    Utilização de Classe forme informando erros

--- backend/appengine/routes/livros.py
+++ backend/appengine/routes/livros.py
@@ -5,6 +5,7 @@ from decimal import Decimal
 from google.appengine.ext import ndb
 from config.template_middleware import TemplateResponse
 from gaecookie.decorator import no_csrf
+from gaeforms import base
 from gaegraph.model import Node
 from gaeforms.ndb import property
 
@@ -15,7 +16,15 @@ from tekton import router
 class Livro(Node):
     titulo = ndb.StringProperty(required=True)
     preco = property.SimpleCurrency(required=True)
-    lancamento = ndb.DateProperty(auto_now_add=True)
+    lancamento = ndb.DateProperty()
+
+
+# Formulários
+
+class LivroForm(base.Form):
+    titulo = base.StringField(required=True)
+    preco = base.DecimalField(required=True, lower=0)
+    lancamento = base.DateField()
 
 
 # Handler de requisições HTTP
@@ -25,7 +34,13 @@ def form():
     return TemplateResponse(contexto)
 
 
-def salvar(titulo, preco, **outras_propriedades):
-    preco = Decimal(preco)
-    livro = Livro(titulo=titulo, preco=preco)
+def salvar(**propriedades):
+    livro_form = LivroForm(**propriedades)
+    erros = livro_form.validate()
+    if erros:
+        contexto = {'salvar_path': router.to_path(salvar),
+                    'erros': erros}
+        return TemplateResponse(contexto, 'livros/form.html')
+    propriedades_transformadas = livro_form.normalize()
+    livro = Livro(**propriedades_transformadas)
     livro.put()
