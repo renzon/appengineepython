d703e2478bcd400ecbd5db2d011f33cbac2d7d43
renzon <renzon@gmail.com>
2014-12-04 21:35:59 -0200

    Configuração de fila "rapida" e utilização de comando para fila de tarefas.

--- backend/appengine/routes/vendas/tasks.py
+++ backend/appengine/routes/vendas/tasks.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 import logging
 from decimal import Decimal
 from google.appengine.api import taskqueue
+from gaebusiness.gaeutil import TaskQueueCommand
 from gaecookie.decorator import no_csrf
 from gaepermission.decorator import login_not_required
 from tekton.router import to_path
@@ -19,6 +20,9 @@ def contabilizar(total='0.00', i='1'):
         params = {'total': '%s' % total,
                   'i': i}
         proximo_passo_path = to_path(contabilizar)
-        taskqueue.add(url=proximo_passo_path, params=params)
+        task_cmd = TaskQueueCommand('rapida',
+                                    proximo_passo_path,
+                                    params=params)
+        task_cmd()
     else:
         logging.info('Contabilização terminada: %s' % total)
