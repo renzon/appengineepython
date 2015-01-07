e09b159710203bc93343de2285ed9374264e7192
renzon <renzon@gmail.com>
2015-01-07 19:00:14 -0200

    Avatar sendo exibido

--- backend/appengine/routes/github.py
+++ backend/appengine/routes/github.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+import json
 from google.appengine.api import urlfetch
+from config.template_middleware import TemplateResponse
 from gaecookie.decorator import no_csrf
 from gaepermission.decorator import login_not_required
 from tekton.router import to_path
@@ -8,8 +10,12 @@ from tekton.router import to_path
 
 @login_not_required
 @no_csrf
-def index(_resp, usuario='renzon'):
+def index(usuario='renzon'):
     url_base = 'https://api.github.com/users'
     url_usuario = to_path(url_base, usuario)
-    resultado = urlfetch.fetch(url=url_usuario)
-    _resp.write(resultado.content)
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    resultado = urlfetch.fetch(url=url_usuario, headers=headers)
+    usuario_dct = json.loads(resultado.content)
+    contexto = {'usuario': usuario,
+                'avatar': usuario_dct['avatar_url']}
+    return TemplateResponse(contexto, 'github.html')
