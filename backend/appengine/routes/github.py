6499d19a6dd665d51da1d19541f750d1f39613f8
renzon <renzon@gmail.com>
2015-01-07 19:16:16 -0200

    Repositórios sendo exibidos

--- backend/appengine/routes/github.py
+++ backend/appengine/routes/github.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 import json
+import logging
 from google.appengine.api import urlfetch
 from config.template_middleware import TemplateResponse
 from gaecookie.decorator import no_csrf
@@ -13,9 +14,15 @@ from tekton.router import to_path
 def index(usuario='renzon'):
     url_base = 'https://api.github.com/users'
     url_usuario = to_path(url_base, usuario)
+    url_repos = to_path(url_usuario, 'repos')
     headers = {'Accept': 'application/vnd.github.v3+json'}
-    resultado = urlfetch.fetch(url=url_usuario, headers=headers)
-    usuario_dct = json.loads(resultado.content)
+    resultado_usuario = urlfetch.fetch(url=url_usuario, headers=headers)
+    resultado_repos = urlfetch.fetch(url=url_repos, headers=headers)
+    usuario_dct = json.loads(resultado_usuario.content)
+    repos_list = json.loads(resultado_repos.content)
+    repos_list = repos_list[:7]
+    logging.info(repr(repos_list))
     contexto = {'usuario': usuario,
-                'avatar': usuario_dct['avatar_url']}
+                'avatar': usuario_dct['avatar_url'],
+                'repos': repos_list}
     return TemplateResponse(contexto, 'github.html')
