7e487ffa433868e5294066c265c0f720d3cecab0
renzon <renzon@gmail.com>
2015-01-07 13:22:06 -0200

    acesso a dados de usuário com urlfetch

--- backend/appengine/routes/github.py
+++ backend/appengine/routes/github.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from google.appengine.api import urlfetch
 from gaecookie.decorator import no_csrf
 from gaepermission.decorator import login_not_required
+from tekton.router import to_path
 
 
 @login_not_required
 @no_csrf
 def index(_resp, usuario='renzon'):
-    _resp.write('Dados do usuário %s do Github' % usuario)
+    url_base = 'https://api.github.com/users'
+    url_usuario = to_path(url_base, usuario)
+    resultado = urlfetch.fetch(url=url_usuario)
+    _resp.write(resultado.content)
