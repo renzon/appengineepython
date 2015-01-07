719f1dad4c28d7bb454b4b2ef1fc9c7440eaf615
renzon <renzon@gmail.com>
2015-01-07 20:05:41 -0200

    Utilização do comando UrlFetch

--- backend/appengine/routes/github.py
+++ backend/appengine/routes/github.py
@@ -4,6 +4,8 @@ import json
 import logging
 from google.appengine.api import urlfetch
 from config.template_middleware import TemplateResponse
+from gaebusiness.business import CommandParallel
+from gaebusiness.gaeutil import UrlFetchCommand
 from gaecookie.decorator import no_csrf
 from gaepermission.decorator import login_not_required
 from tekton.router import to_path
@@ -16,8 +18,11 @@ def index(usuario='renzon'):
     url_usuario = to_path(url_base, usuario)
     url_repos = to_path(url_usuario, 'repos')
     headers = {'Accept': 'application/vnd.github.v3+json'}
-    resultado_usuario = urlfetch.fetch(url=url_usuario, headers=headers)
-    resultado_repos = urlfetch.fetch(url=url_repos, headers=headers)
+    cmd_usuario = UrlFetchCommand(url_usuario, headers=headers)
+    cmd_repos = UrlFetchCommand(url_repos, headers=headers)
+    CommandParallel(cmd_usuario, cmd_repos).execute()
+    resultado_usuario = cmd_usuario.result
+    resultado_repos = cmd_repos.result
     usuario_dct = json.loads(resultado_usuario.content)
     repos_list = json.loads(resultado_repos.content)
     if len(repos_list) > 7:
