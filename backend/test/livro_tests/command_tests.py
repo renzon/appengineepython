4bb98656a977cb4d7f252aa0195d84499ee7f27e
renzon <renzon@gmail.com>
2015-01-20 12:38:14 -0200

    Teste com mommygae

--- backend/test/livro_tests/command_tests.py
+++ backend/test/livro_tests/command_tests.py
@@ -2,9 +2,19 @@
 from __future__ import absolute_import, unicode_literals
 from base import GAETestCase
 from livro_app.livro_commands import BuscarLivroPorIdCmd
+from livro_app.livro_model import Livro
+from mommygae import mommy
+
 
 class BuscarLivroTests(GAETestCase):
     def test_livro_nao_existente(self):
         cmd = BuscarLivroPorIdCmd(1)
         livro = cmd()
         self.assertIsNone(livro)
+
+    def test_livro_existente(self):
+        livro = mommy.save_one(Livro, titulo='App Engine e Python')
+        cmd = BuscarLivroPorIdCmd(livro.key.id())
+        livro_do_bd = cmd()
+        self.assertEqual(livro, livro_do_bd)
+        self.assertEqual(livro.titulo, livro_do_bd.titulo)
