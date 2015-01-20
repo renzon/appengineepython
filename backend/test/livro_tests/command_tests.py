b6488d9e3e1d10838371066f4380e9de994a214a
renzon <renzon@gmail.com>
2015-01-20 12:46:18 -0200

    Teste de lançamento de exceção

--- backend/test/livro_tests/command_tests.py
+++ backend/test/livro_tests/command_tests.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from base import GAETestCase
+from curso_app.curso_model import Curso
+from gaebusiness.business import CommandExecutionException
 from livro_app.livro_commands import BuscarLivroPorIdCmd
 from livro_app.livro_model import Livro
 from mommygae import mommy
@@ -18,3 +20,8 @@ class BuscarLivroTests(GAETestCase):
         livro_do_bd = cmd()
         self.assertEqual(livro, livro_do_bd)
         self.assertEqual(livro.titulo, livro_do_bd.titulo)
+
+    def test_acesso_a_nao_livro(self):
+        curso = mommy.save_one(Curso)
+        cmd = BuscarLivroPorIdCmd(curso)
+        self.assertRaises(CommandExecutionException, cmd)
