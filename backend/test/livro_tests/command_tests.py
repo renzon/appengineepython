efa3514c66798db79d59a17f5db7efc2d682095d
renzon <renzon@gmail.com>
2015-01-20 12:20:13 -0200

    Teste de Busca de livro funcionando

--- backend/test/livro_tests/command_tests.py
+++ backend/test/livro_tests/command_tests.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from base import GAETestCase
-from unittest.case import TestCase
 from livro_app.livro_commands import BuscarLivroPorIdCmd
 
-class BuscarLivroTests(TestCase):
+class BuscarLivroTests(GAETestCase):
     def test_livro_nao_existente(self):
         cmd = BuscarLivroPorIdCmd(1)
         livro = cmd()
