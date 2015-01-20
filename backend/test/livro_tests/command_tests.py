61902f3bd391023659d01397daeb54fdfa62bcca
renzon <renzon@gmail.com>
2015-01-20 12:16:17 -0200

    Teste de Busca de livro com erro de variável global request

---
+++ backend/test/livro_tests/command_tests.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from unittest.case import TestCase
+from livro_app.livro_commands import BuscarLivroPorIdCmd
+
+class BuscarLivroTests(TestCase):
+    def test_livro_nao_existente(self):
+        cmd = BuscarLivroPorIdCmd(1)
+        livro = cmd()
+        self.assertIsNone(livro)
