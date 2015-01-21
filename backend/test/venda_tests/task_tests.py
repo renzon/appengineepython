81571cde438326ec384cfc2dae55850ce0e640ee
renzon <renzon@gmail.com>
2015-01-21 18:15:09 -0200

    Teste de Task funcionando

--- backend/test/venda_tests/task_tests.py
+++ backend/test/venda_tests/task_tests.py
@@ -9,27 +9,38 @@ from venda_app.venda_model import Venda, CONTABILIZADA, NOVA
 
 
 class ContabilizarTests(GAETestCase):
-    @patch('routes.vendas.tasks')
+    @patch('routes.vendas.tasks.TaskQueueCommand')
     def test_contabilizar(self, TaskQueueCommandClassMock):
+        # Setando banco de dados
         Venda(preco=Decimal('0.99'), status=CONTABILIZADA).put()
         vendas_novas = [Venda(preco=Decimal('0.99'), status=NOVA),
                         Venda(preco=Decimal('1.99'), status=NOVA)]
         vendas_chaves = ndb.put_multi(vendas_novas)
+
+        # Criação de Mock para objeto do Tipos TaskQueueCommand
         cmd = Mock()
 
-        def cmd_init(queue, path, params):
-            cmd.reset_mock()
+        subtotais = {'i': 1, 1: '0.99', 2: '2.98'}
 
-            def _contabilizar():
-                contabilizar(params['total'], params['cursor'])
+        def _contabilizar():
+            params = TaskQueueCommandClassMock.call_args[1]['params']
+            subtotal = params['total']
+            i = subtotais['i']
+            self.assertEqual(subtotais[i], subtotal)
+            subtotais['i'] = subtotais['i'] + 1
+            contabilizar(subtotal,
+                         params['cursor'])
 
-            cmd.__call__ = _contabilizar
-            return cmd
+        cmd.side_effect = _contabilizar
 
-        TaskQueueCommandClassMock.__call__ = cmd_init
+        TaskQueueCommandClassMock.return_value = cmd
+
+        # Executanto handler sob teste
         contabilizar()
 
+        # Asserções
         vendas_novas = ndb.get_multi(vendas_chaves)
         vendas_status = [v.status for v in vendas_novas]
-        self.assertListEqual([CONTABILIZADA, CONTABILIZADA],
-                             vendas_status)
+        self.assertListEqual([CONTABILIZADA, CONTABILIZADA], vendas_status)
+        self.assertEqual(2, cmd.call_count)
+
